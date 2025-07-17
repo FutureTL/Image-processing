5ebb2ad11f63cc3689e9f6220058a4380efc0281
FutureTL <futuretechnologistics21@gmail.com>
2025-07-17 11:10:34 +0530

    utils- asyncHandler,ApiResponse, ApiError added

---
+++ src/utlis/asyncHandler.js
@@ -0,0 +1,17 @@
+//asyncHandler is a higher order function that accepts a function as a 
+//parameter, so that we don't have to write try catch every time. Otherwise,
+//I will always have to write my controller functions in a try catch block.
+
+const asyncHandler = (func) => {
+
+     return async(req,res,next)=>{
+
+        try {
+           await func(req, res, next);
+        } catch (error) {
+            console.log("error in asynchandler: ", error);
+        }
+
+     }
+}
+
